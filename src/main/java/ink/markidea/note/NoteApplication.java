3eec4dda99e77fe68752ab6b92c518dfc4fa8fa9
hansanshi <hansanshi@outlook.com>
2020-06-27 23:27:56 +0800

    feat: create project

---
+++ src/main/java/ink/markidea/note/NoteApplication.java
@@ -0,0 +1,13 @@
+package ink.markidea.note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ink/markidea/note/NoteApplicationTests.java
@@ -0,0 +1,13 @@
+package ink.markidea.note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
